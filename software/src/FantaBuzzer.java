5afc9ba30958d5679beece08512c2ba44940aecb
ChrisShow <cristian.maieli@hotmail.it>
2023-12-31 19:17:59 +0100

    pattemu

--- software/src/FantaBuzzer.java
+++ software/src/FantaBuzzer.java
@@ -1,9 +1,14 @@
 import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
 public class FantaBuzzer extends JFrame{
 
     public FantaBuzzer(){
 
-        
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimensioneSchermo = toolkit.getScreenSize();
+        int screenWidth = dimensioneSchermo.width;
+        int screenHeight = dimensioneSchermo.height;
     }
 }
