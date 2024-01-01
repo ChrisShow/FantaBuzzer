52e810e43beae1ad82b0ee3750eb86a9cb1a0680
ChrisShow <cristian.maieli@hotmail.it>
2024-01-01 12:32:51 +0100

    new font imported

--- software/src/FantaBuzzer.java
+++ software/src/FantaBuzzer.java
@@ -2,14 +2,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 
 public class FantaBuzzer extends JFrame{
 
     private JLabel[] labels;
     private JLabel title;
+    private JLabel subtitle;
     private JTextField[] fields;
     private JButton start;
 
@@ -23,18 +27,27 @@ public class FantaBuzzer extends JFrame{
         int width = 600, height = 800;
 
         setBounds((screenWidth - width) / 2, (screenHeight - height) / 2, width, height);
+        setLayout(null);
         setTitle("FantaBuzzer Launcher");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         int distance = 10;
-        int titleWidth = 400, titleHeight = 100;
+        int titleWidth = 300, titleHeight = 100, titleFontSize = 50;
 
         title = new JLabel("FantaBuzzer");
+        title.setFont(caricaFont(titleFontSize));
         title.setBounds((width - titleWidth) / 2, distance, titleWidth, titleHeight);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setVisible(true);
         add(title);
 
-        
+        int subtitleWidth = 500, subtitleHeight = 50;
+
+        subtitle = new JLabel("Inserisci i nomi delle squadre facendoli corrispondere ai numeri di pad");
+        subtitle.setBounds((width - subtitleWidth) / 2, (distance * 2) + titleHeight, subtitleWidth, subtitleHeight);
+        subtitle.setHorizontalAlignment(SwingConstants.CENTER);
+        subtitle.setVisible(true);
+        add(subtitle);
 
         int labelWidth = 100, labelHeight = 50;
         int fieldWidth = 200, fieldHeight = 50;
@@ -43,7 +56,11 @@ public class FantaBuzzer extends JFrame{
         fields = new JTextField[8];
         for (int i = 0; i < fields.length; i++) {
             labels[i] = new JLabel("Squadra " + (i+1) + ":");
-            labels[i].setBounds((width - labelHeight - fieldHeight - (distance * 3) / 2), i, labelWidth, labelHeight);
+            labels[i].setBounds((width - labelWidth - fieldWidth - (distance * 3)) / 2, (distance * (i+4) + titleHeight + subtitleHeight + (labelHeight * i)), labelWidth, labelHeight);
+            labels[i].setVisible(true);
+            labels[i].setHorizontalAlignment(SwingConstants.CENTER);
+            add(labels[i]);
+
             fields[i] = new JTextField();
         }
 
@@ -53,5 +70,17 @@ public class FantaBuzzer extends JFrame{
 
     public static void main(String[] args) {
         FantaBuzzer fantaBuzzer = new FantaBuzzer();
+    }
+
+    private static Font caricaFont(int dimensione) {
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new java.io.File("software/resources/coolveticaRG.otf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+            return font.deriveFont(Font.PLAIN, dimensione);
+        } catch (java.awt.FontFormatException | java.io.IOException e) {
+            e.printStackTrace();
+            return new Font("Arial", Font.PLAIN, 14); // Fallback a un font predefinito in caso di errore
+        }
     }
 }
