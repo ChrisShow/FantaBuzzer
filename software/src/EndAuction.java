c1d76e7fec3f161326e599fd43822906a4c6a7fc
Alessio Bennardo <43845425+Alessiob13@users.noreply.github.com>
2024-01-10 23:40:11 +0100

    EndAuction graphic updates
    graphic updates

--- software/src/EndAuction.java
+++ software/src/EndAuction.java
@@ -1,8 +1,9 @@
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class EndAuction extends JFrame{
 
@@ -12,17 +13,33 @@ public class EndAuction extends JFrame{
         double screenWidth = screenSize.getWidth();
         double screenHeight = screenSize.getHeight();
 
-        int width = 400, height = 600;
+        int width = 500, height = 300;
         setBounds((int)(screenWidth - width) / 2, (int)(screenHeight - height) / 2, width, height);
         setResizable(false);
         setTitle("Asta vinta da " + playerName);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        String s = playerName + " acquisisce le prestazioni sportive di " + footballerName + " per " + offer + " crediti";
-        JLabel winLabel = new JLabel(s);
+        // Carica un'immagine
+        ImageIcon footballerIcon = new ImageIcon("software\\resources\\images\\coin.jpg");
+        // Ridimensiona l'immagine
+        Image img = footballerIcon.getImage();
+        Image newImg = img.getScaledInstance(50, 50, Image.SCALE_SMOOTH); // Imposta le dimensioni desiderate
+        footballerIcon.setImage(newImg);
+
+        String s =  "<html><font color='yellow'>" + playerName + " </font>acquisisce le prestazioni sportive di <font color='yellow'>"
+                + footballerName + "</font> per <font color='yellow'>" + offer + " </font>crediti</html>";
+
+        JLabel winLabel = new JLabel(s,footballerIcon, JLabel.LEFT);
         winLabel.setBounds(10, 10, width - 10, height - 10);
-        winLabel.setFont(UtilityClass.caricaFont(30));
+        winLabel.setFont(UtilityClass.caricaFont(20));
+        winLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        winLabel.setVerticalAlignment(SwingConstants.CENTER);
+        winLabel.setOpaque(true); // Per rendere il colore di sfondo visibile
+        winLabel.setBackground(Color.LIGHT_GRAY); // Imposta il colore di sfondo azzurro
+        winLabel.setForeground(Color.BLUE); // Imposta il colore del testo a bianco
         winLabel.setVisible(true);
+
+        winLabel.setIconTextGap(15); // Imposta lo spazio tra l'icona e il testo
         add(winLabel);
 
 
